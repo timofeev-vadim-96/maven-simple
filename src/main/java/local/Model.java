a30f6cc6c5d6574ac706519647f75587b0135bd8
Vadim Timofeev <timofeev.vadim.96@mail.ru>
2024-03-08 22:14:03 +0300

    initial commit

---
+++ src/main/java/local/Model.java
@@ -0,0 +1,12 @@
+package local;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
+public class Model {
+    private String title;
+}
